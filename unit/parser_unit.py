fd1c5058d484e33483ce0d486faf410548657ff4
Bruce A. MacNaughton <bmacnaughton@gmail.com>
2020-05-11 09:20:31 -0700

    unit/parser_unit.py - skip comment fix

--- unit/parser_unit.py
+++ unit/parser_unit.py
@@ -19,7 +19,7 @@ class HttperfTestCase(unittest.TestCase):
         presults = HttperfParser().parse(httperf_headers_results)
         self.assertEqual(presults, httperf_headers_parsed)
 
-    @unittest.skip('not using jmervine/httperfpy')
+    @unittest.skip('not using jmervine/httperf')
     def testParseVerboseResultCount(self):
         vpresults = HttperfParser().parse(httperf_verbose_results)
         self.assertEqual(len(vpresults.keys()), 57)
